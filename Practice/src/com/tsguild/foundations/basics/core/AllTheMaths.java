7f5d56d4d18f23f019313879ba0f8a80699afecd
Taylor Lapointe <Taylor.Lapointe@LibertyMutual.com>
2018-01-23 09:50:57 -0500

    practice problems

---
+++ Practice/src/com/tsguild/foundations/basics/core/AllTheMaths.java
@@ -0,0 +1,8 @@
+package com.tsguild.foundations.basics.core;
+
+public class AllTheMaths {
+    public static void main(String[] args) {
+        System.out.println("1 + 2 is: ");
+
+    }
+}
---
+++ Practice/src/com/tsguild/foundations/basics/core/ProjectGutenberg.java
@@ -0,0 +1,12 @@
+package com.tsguild.foundations.basics.core;
+
+public class ProjectGutenberg {
+    public static void main(String[] args) {
+        System.out.println("Did you know that in 1440 (or thereabouts)");
+        System.out.println("Johannes Gutenberg invented the printing press?");
+        System.out.println("He started out as a goldsmith!");
+        System.out.println("His invention made rapid and prolific typed volumes Available for the first ever ...!");
+        System.out.println("We are like a modern gutenberg!");
+        System.out.println("Printing vast amounts to the waiting console with ease!");
+    }
+}
