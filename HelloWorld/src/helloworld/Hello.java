ee33a9c430f51309d8c9dffc93583c926403d24f
Taylor Lapointe <Taylor.Lapointe@LibertyMutual.com>
2018-01-23 17:10:17 -0500

    TAYLORLAPOINTEINDIVIDUALWORK-2 fixed spelling error in Hello World

--- HelloWorld/src/helloworld/Hello.java
+++ HelloWorld/src/helloworld/Hello.java
@@ -2,22 +2,7 @@ package helloworld;
 
 public class Hello {
     public static void main(String[] args){
-//        System.out.println("Hello Worlddd!");
-
-
-        int counter;  // initializing a variable
-        counter = 7;  // assignment
-        System.out.println(counter);
-
-        counter = 45;
-        System.out.println(counter);
-
-        // not allowed, too big for type int: counter = 214748364700
-
-        boolean playAgain = false;
-        playAgain = true;
-
-        char theLetterX = 'x';  // use single quotes only for char
+        System.out.println("Hello World!");
 
     }
 }
