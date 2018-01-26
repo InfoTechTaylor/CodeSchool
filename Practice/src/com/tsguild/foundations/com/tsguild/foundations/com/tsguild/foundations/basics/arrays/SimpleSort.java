d1a75c8bc65d79e230a357dc7fa822205eb5829e
Taylor Lapointe <Taylor.Lapointe@LibertyMutual.com>
2018-01-26 14:03:49 -0500

    TAYLORLAPOINTEINDIVIDUALWORK-11 completed RockPaperScissors class for cummulative assessment

---
+++ Practice/src/com/tsguild/foundations/com/tsguild/foundations/com/tsguild/foundations/basics/arrays/SimpleSort.java
@@ -0,0 +1,16 @@
+package com.tsguild.foundations.com.tsguild.foundations.com.tsguild.foundations.basics.arrays;
+
+public class SimpleSort {
+    public static void main(String[] args) {
+        int[] firstHalf = {3, 7, 9, 10, 16, 19, 20, 34, 55, 67, 88, 99};
+        int[] secondHalf = {1, 4, 8, 11, 15, 18, 21, 44, 54, 79, 89, 100};
+
+        int[] wholeNumbers = new int[24];
+
+        // Sorting code should go here!
+        for(int i=0; i <wholeNumbers.length; i++){
+
+            //for(int)
+        }
+    }
+}
