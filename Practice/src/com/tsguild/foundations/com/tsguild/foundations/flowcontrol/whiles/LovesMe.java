ecdea4f1eb6bda61f627879024533c31f6117f06
Taylor Lapointe <Taylor.Lapointe@LibertyMutual.com>
2018-01-28 18:51:45 -0500

    TAYLORLAPOINTEINDIVIDUALWORK-6 start and completion of LovesMe class

---
+++ Practice/src/com/tsguild/foundations/com/tsguild/foundations/flowcontrol/whiles/LovesMe.java
@@ -0,0 +1,22 @@
+package com.tsguild.foundations.com.tsguild.foundations.flowcontrol.whiles;
+
+public class LovesMe {
+    public static void main(String[] args) {
+        System.out.println("Well here goes nothing...");
+
+        int petalCount = 1;
+        while(petalCount <= 34){ // I chose a while instead of a do while because I only want it to execute if there are petals. 
+            if(petalCount % 2 != 0){
+                System.out.println("It loves me NOT!");
+            } else {
+                System.out.println("It LOVES me!");
+
+                if(petalCount == 34){
+                    System.out.println("I knew it! It LOVES ME!");
+                }
+            }
+
+            petalCount++;
+        }
+    }
+}
