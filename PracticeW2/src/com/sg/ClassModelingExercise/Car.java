89b8e244df96e16a8bb007f39fcf48cac3ee9e96
Taylor Lapointe <Taylor.Lapointe@LibertyMutual.com>
2018-01-29 21:06:21 -0500

    TAYLORLAPOINTEINDIVIDUALWORK-16 started Car and Car2 classes, put attributes, constructor, getters/setters, and methods

--- PracticeW2/src/com/sg/ClassModelingExercise/Car.java
+++ PracticeW2/src/com/sg/ClassModelingExercise/Car.java
@@ -1,4 +1,67 @@
 package com.sg.ClassModelingExercise;
 
 public class Car {
+
+    private String make;
+    private String model;
+    private int year;
+    private String transmission;
+    private int vin;
+    private int numDoors;
+
+    public Car(String make, String model, int year, String transmission, int vin){
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.transmission = transmission;
+        this.vin = vin;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public int getVin() {
+        return vin;
+    }
+
+    public void setVin(int vin) {
+        this.vin = vin;
+    }
+
+    public int getNumDoors() {
+        return numDoors;
+    }
+
+    public void setNumDoors(int numDoors) {
+        this.numDoors = numDoors;
+    }
 }
