77d22e5c36727fdcbc3867d184f54bc4b4d581d4
Taylor Lapointe <Taylor.Lapointe@LibertyMutual.com>
2018-02-01 15:18:12 -0500

    TAYLORLAPOINTEINDIVIDUALWORK-19 implemented structure of MVP student rostser app completed with exception handling for the code along

--- ClassRoster/src/com/sg/classroster/controller/ClassRosterController.java
+++ ClassRoster/src/com/sg/classroster/controller/ClassRosterController.java
@@ -1,47 +1,90 @@
 package com.sg.classroster.controller;
 
-import com.sg.classroster.ui.UserIO;
-import com.sg.classroster.ui.UserIOConsoleImpl;
+import com.sg.classroster.dao.ClassRosterDao;
+import com.sg.classroster.dao.ClassRosterDaoException;
+import com.sg.classroster.dto.Student;
+import com.sg.classroster.ui.ClassRosterView;
+import java.util.List;
 
 public class ClassRosterController {
+    ClassRosterView view;
+    ClassRosterDao dao;
 
-    private UserIO io = new UserIOConsoleImpl();
+    public ClassRosterController(ClassRosterDao dao, ClassRosterView view){
+        this.dao = dao;
+        this.view = view;
+    }
+
+    private void createStudent() throws ClassRosterDaoException {
+        view.displayCreateStudentBanner();
+        Student newStudent = view.getNewStudentInfo();
+        dao.addStudent(newStudent.getStudentId(), newStudent);
+        view.displayCreateStudentBanner();
+    }
 
     public void run() {
         boolean keepGoing = true;
         int menuSelection = 0;
-        while (keepGoing) {
-            io.print("Main Menu");
-            io.print("1. List Student IDs");
-            io.print("2. Create New Student");
-            io.print("3. View a Student");
-            io.print("4. Remove a Student");
-            io.print("5. Exit");
+        try {
+            while (keepGoing) {
+                menuSelection = getMenuSelection();
 
-            menuSelection = io.readInt("Please select from the"
-                    + " above choices.", 1, 5);
+                switch (menuSelection) {
+                    case 1:
+                        listStudents();
+                        break;
+                    case 2:
+                        createStudent();
+                        break;
+                    case 3:
+                        viewStudent();
+                        break;
+                    case 4:
+                        removeStudent();
+                        break;
+                    case 5:
+                        keepGoing = false;
+                        break;
+                    default:
+                        unknownCommand();
+                }
 
-            switch (menuSelection) {
-                case 1:
-                    io.print("LIST STUDENTS");
-                    break;
-                case 2:
-                    io.print("CREATE STUDENT");
-                    break;
-                case 3:
-                    io.print("VIEW STUDENT");
-                    break;
-                case 4:
-                    io.print("REMOVE STUDENT");
-                    break;
-                case 5:
-                    keepGoing = false;
-                    break;
-                default:
-                    io.print("UNKNOWN COMMAND");
             }
-
+            exitMessage();
+        } catch (ClassRosterDaoException e) {
+            view.displayErrorMessage(e.getMessage());
         }
-        io.print("GOOD BYE");
+    }
+
+    private int getMenuSelection() {
+        return view.printMenuAndGetSelection();
+    }
+
+    private void listStudents() throws ClassRosterDaoException {
+        view.displayDisplayAllBanner();
+        List<Student> studentList = dao.getAllStudents();
+        view.displayStudentList(studentList);
+    }
+
+    private void viewStudent() throws ClassRosterDaoException{
+        view.displayDisplayStudentBanner();
+        String studentId = view.getStudentIdChoice();
+        Student student = dao.getStudent(studentId);
+        view.displayStudent(student);
+    }
+
+    private void removeStudent() throws ClassRosterDaoException {
+        view.displayRemoveStudentBanner();
+        String studentId = view.getStudentIdChoice();
+        dao.removeStudent(studentId);
+        view.displayRemoveSuccessBanner();
+    }
+
+    private void unknownCommand(){
+        view.displayUnknownCommandBanner();
+    }
+
+    private void exitMessage(){
+        view.displayExitBanner();
     }
 }
