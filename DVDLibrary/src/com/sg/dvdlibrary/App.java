e1227e4658b37f66d33610af6d9c7680cc01aa58
Taylor Lapointe <Taylor.Lapointe@LibertyMutual.com>
2018-02-02 11:19:37 -0500

    TAYLORLAPOINTEINDIVIDUALWORK-35 added switch statement in main with static print statements to test menu is working in a while loop, call the controller run menu from App

--- DVDLibrary/src/com/sg/dvdlibrary/App.java
+++ DVDLibrary/src/com/sg/dvdlibrary/App.java
@@ -1,7 +1,10 @@
 package com.sg.dvdlibrary;
 
+import com.sg.dvdlibrary.controller.DvdController;
+
 public class App {
     public static void main(String[] args) {
-
+        DvdController controller = new DvdController();
+        controller.run();
     }
 }
--- DVDLibrary/src/com/sg/dvdlibrary/controller/DvdController.java
+++ DVDLibrary/src/com/sg/dvdlibrary/controller/DvdController.java
@@ -9,6 +9,46 @@ public class DvdController {
 
 
     public void run() {
-        boolean 
+        boolean keepGoing = true;
+        int menuSelection = 0;
+
+        while(keepGoing){
+            io.print("Main Menu:");
+            io.print("\t1. Add a DVD to the collection");
+            io.print("\t2. Remove a DVD from the collection");
+            io.print("\t3. Edit the information for an existing DVD in the collection");
+            io.print("\t4. List all DVDs in the collection");
+            io.print("\t5. Search for a DVD by title");
+            io.print("\t6. Exit program");
+
+            menuSelection = io.readInt("Please select one of the above options: ");
+
+            // determine action based of menuSelection
+            switch(menuSelection){
+                case 1:
+                    io.print("ADD DVD");
+                    break;
+                case 2:
+                    io.print("REMOVE DVD");
+                    break;
+                case 3:
+                    io.print("EDIT DVD");
+                    break;
+                case 4:
+                    io.print("LIST ALL DVDs");
+                    break;
+                case 5:
+                    io.print("SEARCH FOR DVD");
+                    break;
+                case 6:
+                    keepGoing = false;
+                    io.print("GOOD BYE");
+                default:
+                    io.print("UNKNOWN COMMAND");
+            } // end switch(menuSelection)
+
+        } // end while(keepGoing)
+
     } // end run()
+
 } // end DvdController
