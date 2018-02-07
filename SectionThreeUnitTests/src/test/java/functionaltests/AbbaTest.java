5c15db57b0ba3247e1aaa30823926b9090434a94
Taylor Lapointe <Taylor.Lapointe@LibertyMutual.com>
2018-02-07 18:06:12 -0500

    TAYLORLAPOINTEINDIVIDUALWORK-41 implemented Abba class and the AbbaTest class with three unit tests that all pass

---
+++ SectionThreeUnitTests/src/test/java/functionaltests/AbbaTest.java
@@ -0,0 +1,46 @@
+package functionaltests;
+
+//import org.junit.After;
+//import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AbbaTest {
+
+    Abba abba = new Abba();
+
+    public AbbaTest() {
+
+    }
+
+//    @Before
+//    public void setUp() throws Exception {
+//
+//    }
+//
+//    @After
+//    public void tearDown() throws Exception {
+//    }
+
+
+
+
+    @Test
+    public void testAbbaHiBye() {
+        // abba("Hi", "Bye") -> "HiByeByeHi"
+        assertEquals(abba.abba("Hi", "Bye"), "HiByeByeHi");
+    }
+
+    @Test
+    public void testAbbaYoAlice() {
+        // abba("Yo", "Alice") -> "YoAliceAliceYo"
+        assertEquals(abba.abba("Yo", "Alice"), "YoAliceAliceYo");
+    }
+
+    @Test
+    public void testAbbaWhatUp() {
+        // abba("What", "Up") -> "WhatUpUpWhat"
+        assertEquals(abba.abba("What", "Up"), "WhatUpUpWhat");
+    }
+}
