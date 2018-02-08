8e286fcd64634564f04676e4fbb2efa6a4984113
Taylor Lapointe <Taylor.Lapointe@LibertyMutual.com>
2018-02-07 20:00:50 -0500

    TAYLORLAPOINTEINDIVIDUALWORK-41 started MakePi class and unit tests, not currently working as I haven't thought of a good way to take the results of Math.PI and only add the provided number, n, of ints to the int array.

---
+++ SectionThreeUnitTests/src/main/java/functionaltests/MakePi.java
@@ -0,0 +1,20 @@
+package functionaltests;
+
+public class MakePi {
+    // Return an int array length n containing the first n digits of pi.
+    //
+    // makePi(3) -> {3, 1, 4}
+
+    public int[] makePi(int n) {
+//        throw new UnsupportedOperationException("Not implemented");
+        int[] array = new int[n];
+        double pi = Math.PI;
+        String piString = Double.toString(pi);
+
+        for(int i = 0; i < array.length; i++){
+
+        }
+        return array;
+
+    }
+}
---
+++ SectionThreeUnitTests/src/test/java/functionaltests/MakePiTest.java
@@ -0,0 +1,23 @@
+package functionaltests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MakePiTest {
+    private MakePi pi = new MakePi();
+
+    public MakePiTest(){
+
+    }
+
+    @Test
+    public void makePi() {
+        // Return an int array length n containing the first n digits of pi.
+        //
+        // makePi(3) -> {3, 1, 4}
+
+        int[] piArray = {3,1,4};
+        assertEquals(pi.makePi(3), piArray);
+    }
+}
