f9de277b04658a596496d094bf94ee2213ded713
Taylor Lapointe <Taylor.Lapointe@LibertyMutual.com>
2018-02-09 10:37:17 -0500

    TAYLORLAPOINTEINDIVIDUALWORK-44 created test class for VendingMachineServiceLayer, just with shells of the test methods for now

---
+++ VendingMachine/src/test/java/service/VendingMachineServiceLayerTest.java
@@ -0,0 +1,38 @@
+package service;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VendingMachineServiceLayerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void retrieveAllVendingMachineItems() {
+    }
+
+    @Test
+    public void addMoneyToMemory() {
+    }
+
+    @Test
+    public void purchaseItem() {
+    }
+
+    @Test
+    public void convertDollarsToChange() {
+    }
+
+    @Test
+    public void retrieveRemainingMoney() {
+    }
+}
