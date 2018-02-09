7334e19b41abab65214f612209eabd9313643297
Taylor Lapointe <Taylor.Lapointe@LibertyMutual.com>
2018-02-09 10:35:25 -0500

    TAYLORLAPOINTEINDIVIDUALWORK-51 implemented loadVendingMachineItems() method to load items from the text file into memory (into the Map)

--- VendingMachine/src/main/java/dao/VendingMachineDaoFileImpl.java
+++ VendingMachine/src/main/java/dao/VendingMachineDaoFileImpl.java
@@ -5,6 +5,7 @@ import dto.VendingMachineItem;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -48,8 +49,41 @@ public class VendingMachineDaoFileImpl implements VendingMachineDao {
     }
 
     private void loadVendingMachineItems() throws VendingMachinePersistenceException{
+        Scanner scanner;
 
-    }
+        try{
+            // create Scanner for reading the file
+            scanner = new Scanner(new BufferedReader(new FileReader(VENDING_MACHINE_ITEMS_TEXT)));
+        } catch(FileNotFoundException e) {
+            throw new VendingMachinePersistenceException("-_- Could not load dvd data into memory.", e);
+        }// end catch
+
+        // currentLine holds the most recent line read from the file
+        String currentLine;
+
+        // currentToken holds each of the parts of the currentLine after it has been split on our DELIMITER
+        String[] currentTokens;
+
+        // Go through each line in VENDING_MACHINE_ITEMS_TEXT, decoding each line into a VendingMachineItem object
+        // process while we have more lines in the file
+        while(scanner.hasNextLine()) {
+            // get the next line in the file
+            currentLine = scanner.nextLine();
+            // break up the line into tokens
+            currentTokens = currentLine.split(STRING_DELIMITER);
+            // create a new VendingMachineItem object and put it into the map vendingMachineItemMap
+            VendingMachineItem currentItem = new VendingMachineItem(currentTokens[0]);
+            // set the remaining values on currentItem manually
+            currentItem.setItemName(currentTokens[1]);
+            currentItem.setItemCost(new BigDecimal(currentTokens[2]));
+            currentItem.setItemQuantity(Integer.parseInt(currentTokens[3]));
+
+            // put currentItem into the map using itemId as the key
+            vendingMachineItemMap.put(currentItem.getItemId(), currentItem);
+        } // end while(scanner.hasNextLine())
+        //close scanner
+        scanner.close();
+    } // end load method
 
     private void writeVendingMachineItems() throws VendingMachinePersistenceException{
 
