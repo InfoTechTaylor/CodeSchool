ad3456cef44b73a649f6fdf0cf84e6bbbeb465a5
Taylor Lapointe <Taylor.Lapointe@LibertyMutual.com>
2018-02-09 10:36:50 -0500

    TAYLORLAPOINTEINDIVIDUALWORK-43 created test class for VendingMachineDao, just with shells of the test methods for now

---
+++ VendingMachine/src/test/java/dao/VendingMachineDaoTest.java
@@ -0,0 +1,38 @@
+package dao;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VendingMachineDaoTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void retrieveAllVendingMachineItems() {
+    }
+
+    @Test
+    public void updateItem() {
+    }
+
+    @Test
+    public void retrieveItemById() {
+    }
+
+    @Test
+    public void removeVendingMachineItem() {
+    }
+
+    @Test
+    public void createVendingMachineItem() {
+    }
+}
