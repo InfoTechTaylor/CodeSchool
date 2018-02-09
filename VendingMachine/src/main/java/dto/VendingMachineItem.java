899d8b8025c30bf3351e1527d8f9bb2cd0cdbfa7
Taylor Lapointe <Taylor.Lapointe@LibertyMutual.com>
2018-02-08 20:09:36 -0500

    TAYLORLAPOINTEINDIVIDUALWORK-60 implemented parameters and getters/setters for VendingMachineChange and VendingMachineItem classes. Added a constructor for VendingMachineItem that accepts an itemId and removed the setter for itemId to keep read only

--- VendingMachine/src/main/java/dto/VendingMachineItem.java
+++ VendingMachine/src/main/java/dto/VendingMachineItem.java
@@ -1,4 +1,45 @@
 package dto;
 
+import java.math.BigDecimal;
+
 public class VendingMachineItem {
+
+    private String itemId;
+    private String itemName;
+    private BigDecimal itemCost;
+    private int itemQuantity;
+
+    public VendingMachineItem(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+// KEEP NO SETTER FOR itemId so that it is read only
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public BigDecimal getItemCost() {
+        return itemCost;
+    }
+
+    public void setItemCost(BigDecimal itemCost) {
+        this.itemCost = itemCost;
+    }
+
+    public int getItemQuantity() {
+        return itemQuantity;
+    }
+
+    public void setItemQuantity(int itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
 }
