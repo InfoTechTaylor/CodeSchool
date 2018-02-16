977ea07212a105082c8d88cb968bb4d9fb80232a
Taylor Lapointe <Taylor.Lapointe@LibertyMutual.com>
2018-02-15 19:51:14 -0500

    TAYLORLAPOINTEINDIVIDUALWORK-66 adding a logging aspect to Vending Machine that logs exceptions thrown on addMoneyToMemory, get change, and purchaseItem methods

--- VendingMachine/src/main/java/advice/LoggingAdvice.java
+++ VendingMachine/src/main/java/advice/LoggingAdvice.java
@@ -3,6 +3,9 @@ package advice;
 import dao.VendingMachineAuditDao;
 import dao.VendingMachinePersistenceException;
 import org.aspectj.lang.JoinPoint;
+import service.InsufficientFundsException;
+import service.InvalidAmountException;
+import service.NoItemInventoryException;
 
 public class LoggingAdvice {
 
@@ -12,12 +15,19 @@ public class LoggingAdvice {
         this.auditDao = auditDao;
     }
 
+    // creates an audit entry for a successful method call
     public void createAuditEntry(JoinPoint jp) {
         Object[] args = jp.getArgs();
         String auditEntry = jp.getSignature().getName() + ": ";
 
+        String itemInfo="";
         for (Object currentArg : args) {
-            auditEntry += currentArg;
+            itemInfo += currentArg;
+        }
+        if(itemInfo.equals("")){
+            auditEntry += "successful.";
+        } else {
+            auditEntry += " user input: " + itemInfo + " :successful.";
         }
         try {
             auditDao.writeAuditEntry(auditEntry);
@@ -25,4 +35,30 @@ public class LoggingAdvice {
             System.err.println("ERROR: Could not create audit entry in LoggingAdvice.");
         }
     }
+
+    // creates an audit entry for when an exception is thrown when method is called
+    public void createExceptionAuditEntry(JoinPoint jp, Exception e){
+            Object[] args = jp.getArgs(); // args are the parameters of the JoinPoint (method called)
+
+            String auditEntry;
+            String itemInfo = "";
+            for (Object currentArg : args) {
+                itemInfo += currentArg;
+            }
+
+            if (itemInfo != "") {
+                auditEntry = jp.getSignature().getName() + ": user input: " + itemInfo + " :Throws " + e.toString();
+            } else {
+                auditEntry = jp.getSignature().getName() +  " :Throws " + e.toString();
+            }
+
+            try {
+//            auditDao.writeAuditEntry("There has been an exception for the above method call: " + e.toString());
+                auditDao.writeAuditEntry(auditEntry);
+            } catch (VendingMachinePersistenceException ex) {
+                System.err.println("ERROR: Could not create audit entry in LoggingAdvice.");
+            }
+
+    }
+
 }
