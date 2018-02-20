954bc2c7c1c4e9f45e7188796410c93242ffec79
Taylor Lapointe <Taylor.Lapointe@LibertyMutual.com>
2018-02-20 13:45:07 -0500

    TAYLORLAPOINTEINDIVIDUALWORK-85 Flooring MasteryTAYLORLAPOINTEINDIVIDUALWORK-96 implemented three DTOs for Order, Product, and Tax

--- FlooringMastery/src/main/java/dto/Order.java
+++ FlooringMastery/src/main/java/dto/Order.java
@@ -1,4 +1,98 @@
 package dto;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class Order {
+
+    private int orderNumber;
+    private LocalDate orderDate;
+    private String customerName;
+    private Tax taxObject;
+    private Product productObject;
+    private BigDecimal area;
+    private BigDecimal totalMaterialCost;
+    private BigDecimal totalLaborCost;
+    private BigDecimal totalTax;
+    private BigDecimal totalCost;
+
+    public int getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(int orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public Tax getTaxObject() {
+        return taxObject;
+    }
+
+    public void setTaxObject(Tax taxObject) {
+        this.taxObject = taxObject;
+    }
+
+    public Product getProductObject() {
+        return productObject;
+    }
+
+    public void setProductObject(Product productObject) {
+        this.productObject = productObject;
+    }
+
+    public BigDecimal getArea() {
+        return area;
+    }
+
+    public void setArea(BigDecimal area) {
+        this.area = area;
+    }
+
+    public BigDecimal getTotalMaterialCost() {
+        return totalMaterialCost;
+    }
+
+    public void setTotalMaterialCost(BigDecimal totalMaterialCost) {
+        this.totalMaterialCost = totalMaterialCost;
+    }
+
+    public BigDecimal getTotalLaborCost() {
+        return totalLaborCost;
+    }
+
+    public void setTotalLaborCost(BigDecimal totalLaborCost) {
+        this.totalLaborCost = totalLaborCost;
+    }
+
+    public BigDecimal getTotalTax() {
+        return totalTax;
+    }
+
+    public void setTotalTax(BigDecimal totalTax) {
+        this.totalTax = totalTax;
+    }
+
+    public BigDecimal getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(BigDecimal totalCost) {
+        this.totalCost = totalCost;
+    }
 }
--- FlooringMastery/src/main/java/dto/Product.java
+++ FlooringMastery/src/main/java/dto/Product.java
@@ -1,4 +1,39 @@
 package dto;
 
+import java.math.BigDecimal;
+
 public class Product {
+    private String productType;
+    private BigDecimal materialCostPerSquareFoot;
+    private BigDecimal laborCostPerSquareFoot;
+
+    public Product(String productType, BigDecimal materialCostPerSquareFoot, BigDecimal laborCostPerSquareFoot) {
+        this.productType = productType;
+        this.materialCostPerSquareFoot = materialCostPerSquareFoot;
+        this.laborCostPerSquareFoot = laborCostPerSquareFoot;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public BigDecimal getMaterialCostPerSquareFoot() {
+        return materialCostPerSquareFoot;
+    }
+
+    public BigDecimal getLaborCostPerSquareFoot() {
+        return laborCostPerSquareFoot;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public void setMaterialCostPerSquareFoot(BigDecimal materialCostPerSquareFoot) {
+        this.materialCostPerSquareFoot = materialCostPerSquareFoot;
+    }
+
+    public void setLaborCostPerSquareFoot(BigDecimal laborCostPerSquareFoot) {
+        this.laborCostPerSquareFoot = laborCostPerSquareFoot;
+    }
 }
