1c0df2222386edecc42da4b0778f4538f93de1fe
Taylor Lapointe <Taylor.Lapointe@LibertyMutual.com>
2018-02-28 12:28:59 -0500

    testing git config settings with px

--- BaseballLeague/src/main/java/dto/TESTDTO.java
+++ BaseballLeague/src/main/java/dto/TESTDTO.java
@@ -4,5 +4,6 @@ public class TESTDTO {
     public static void main(String[] args) {
         System.out.println("TEST");
         System.out.println("new");
+        System.out.println("new");
     }
 }
