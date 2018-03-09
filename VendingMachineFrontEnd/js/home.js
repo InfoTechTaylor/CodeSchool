36000e3593610f831bd136fb8a8b994ed1a1e761
Taylor Lapointe <Taylor.Lapointe@LibertyMutual.com>
2018-03-09 16:26:14 -0500

    TAYLORLAPOINTEINDIVIDUALWORK-99 added purchase item and get change

--- VendingMachineFrontEnd/js/home.js
+++ VendingMachineFrontEnd/js/home.js
@@ -11,7 +11,6 @@ $(document).ready(function() {
     // on add quarter button
     $('#addQuarterBtn').click(function(){
        addMoney(.25);
-
     }); // end on click addQuarterBtn
 
     // on click add dime button
@@ -23,6 +22,19 @@ $(document).ready(function() {
     $('#addNickelBtn').click(function(){
         addMoney(.05);
     }); // end on click addNickelBtn
+
+    // make purchase on click
+    $('#makePurchaseBtn').click(function() {
+        var usersChoice = $('#itemText').val();
+        purchaseItem(usersChoice);
+    });
+
+    // get change on click
+    $('#changeBtn').click(function(){
+       getChange();
+        $('#currentMoneyAmt').val('');
+        $('#itemText').val('');
+    });
 
 }); // end document ready function
 
@@ -46,11 +58,13 @@ function loadInventory(){
         error: function(){
             alert('FAILURE');
         }
-    }) // end get all inventory
+    }); // end get all inventory ajax call
 } // end loadInventory()
 
 function clearTextBoxes(){
     $('#currentMoneyAmt').val('');
+    $('#itemText').val('');
+    $('#changeText').val('');
 }
 
 function addMoney(amount){
@@ -58,4 +72,63 @@ function addMoney(amount){
     currentAmt = +currentAmt + amount;
     $('#currentMoneyAmt').val(currentAmt.toFixed(2));
 }
+
+function purchaseItem(usersChoice){
+
+    var currentAmount = $('#currentMoneyAmt').val();
+
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8080/money/' + currentAmount + '/item/' + usersChoice,
+        success: function(){
+            alert("SUCCESS");
+            // update current amount in the total $ in text box
+            // api call returns change, make method to convert change to dollars
+        },
+        error: function(error){
+            console.log(error.responseJSON.message);
+            alert(error.message);
+
+            // handle no inventory error
+
+            // handle insufficient funds error
+        }
+    }); // end
+    $('#vendingMachineItemRowDiv').empty();
+    loadInventory();
+}
+
+function getChange(){
+    var currentAmt = $('#currentMoneyAmt').val();
+    var currentAmtAsPennies = +currentAmt * 100;
+    var numQuarters = 0;
+    var numDimes = 0;
+    var numNickels = 0;
+    var numPennies = 0;
+    var originalAmtOfPennies = 0;
+
+    // get num quarters
+    if(currentAmtAsPennies >= 25){
+        originalAmtOfPennies = currentAmtAsPennies;
+        currentAmtAsPennies = currentAmtAsPennies % 25;
+        numQuarters = (originalAmtOfPennies - currentAmtAsPennies) / 25;
+    }
+
+    if(currentAmtAsPennies >= 10){
+        originalAmtOfPennies = currentAmtAsPennies;
+        currentAmtAsPennies = currentAmtAsPennies % 10;
+        numDimes = (originalAmtOfPennies - currentAmtAsPennies) / 10;
+    }
+
+    if(currentAmtAsPennies >= 5){
+        originalAmtOfPennies = currentAmtAsPennies;
+        currentAmtAsPennies = currentAmtAsPennies % 5;
+        numNickels = (originalAmtOfPennies - currentAmtAsPennies) / 5;
+    }
+
+    numPennies = currentAmtAsPennies;
+
+    $('#changeText').val('Quarters: ' + numQuarters + '\nDimes: ' + numDimes + '\nNickels: ' + numNickels + '\nPennies: ' + numPennies);
+}
+
 
