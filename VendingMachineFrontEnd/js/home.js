a87251c0b03d636fb5514ad2c5b1e77df64a2153
Taylor Lapointe <Taylor.Lapointe@LibertyMutual.com>
2018-03-09 13:48:07 -0500

    TAYLORLAPOINTEINDIVIDUALWORK-99 created loadInventory() function in js file to load in vending machine items and create divs dynamically

--- VendingMachineFrontEnd/js/home.js
+++ VendingMachineFrontEnd/js/home.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
+    loadInventory();
 });
 
+function loadInventory(){
+
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8080/items',
+        success: function(inventoryArray){
+            // loop through all items in the inventory
+            $.each(inventoryArray, function(index, inventoryItem){
+                // create a new panel div for each item in the inventory
+                $('#vendingMachineItemRowDiv').append('<div class="col-lg-4 col-sm-4 panel panel-primary">' +
+                    '<p class="text-left">' + inventoryItem.id + '</p>' +
+                    '<p class="text-center">' + inventoryItem.name + '</p>' +
+                    '<p class="text-center">$' + inventoryItem.price + '</p>' +
+                    '<p class="text-center">Quantity Left: '+ inventoryItem.quantity +'</p></div>');
+            });
+        },
+        error: function(){
+            alert('FAILURE');
+        }
+    })
+}
 
